e987b8e1849897880d03085814d1bd2436bab5a9
unknown <aly@yandex.ru>
2022-12-07 15:34:14 +0300

    задача43

---
+++ task18/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+int GetNumberFromUser(string message)
+{
+    int result = 0;
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+    double InterSectionPoint (double b1, double k1, double b2, double k2)
+{
+            
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения двух прямых имеет координаты ({x};{y})");
+    return x;
+}
+Console.WriteLine("Введите значения переменных b1, k1, b2, k2 для нахождения точки пересечения ");
+Console.WriteLine("двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2");
+int b1 = GetNumberFromUser ("Введите значение b1: ");
+int k1 = GetNumberFromUser ("Введите значение k1: ");
+int b2 = GetNumberFromUser ("Введите значение b2: ");
+int k2 = GetNumberFromUser ("Введите значение k2: ");
+double result = InterSectionPoint (b1, k1, b2, k2);
